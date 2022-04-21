2fbf71f0cc98edf1233789313d4b82b31987706e
kullaikullai7169 <kullaikullai7169@gmail.com>
2022-04-21 15:44:23 +0530

    kullai2

--- kullai2.rb
+++ kullai2.rb
@@ -1,1 +1,22 @@
-puts"hlo ruby"
+a = 30
+b = 40
+puts "a + b is{b+a}"
+a = 10
+b = 10.23
+c = "fosvsva"
+# a , b , c = 10, 10.23, "safsadf"
+
+# a is 10 and its class is integer
+puts "a is #{a} and its class is #{a.class}"
+puts "b is #{b} and its class is #{b.class}"
+puts "c is #{c} and its class is #{c.class}"
+#kullai ruby
+a = 30
+b = 45.34
+c = "fosvsva"
+# a , b , c = 30, 45.34, "safsadf"
+
+# a is 30 and its class is integer
+puts "a is #{a} and its class is #{a.class}"
+puts "b is #{b} and its class is #{b.class}"
+puts "c is #{c} and its class is #{c.class}"
---
+++ kullai4.rb
@@ -0,0 +1,8 @@
+i = 5
+num = 15
+while i < num  do
+   puts("Inside the loop i = #{i}" )
+   i +=1
+end
+
+
