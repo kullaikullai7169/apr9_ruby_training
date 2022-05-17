42e0fa2ebf3f3bb3ccd3e2b22d37ce563147901a
kullaikullai7169 <kullaikullai7169@gmail.com>
2022-05-18 02:05:21 +0530

    .rb

---
+++ rob.rb
@@ -0,0 +1,81 @@
+# #****************** method demo class1******************
+
+# def ruby1(b, d, a)
+# 	puts a,d 
+# end
+
+# def swamy1(*d)
+# 	puts d.class
+
+# d.each do |ele|
+# 	puts ele *8
+# end
+# end 
+
+# puts ruby1(12,34,56)
+# puts swamy1(67,78,90)
+
+ 
+# def kullai1(z=7,x=6,c)
+# 	puts z
+#     puts x
+#     puts c 
+
+# end 
+
+
+# kullai1(1,9)
+
+
+
+
+#*******************return method *************
+
+# def swamy1(*d)
+# 	puts d.class
+
+# d.each do |ele|
+# 	 ele *8
+# end
+# 	return d
+# 	puts ahshjaka
+
+
+# end 
+
+# puts swamy1(23,45,67,89)
+
+
+
+
+#  def ruby1(b, d, a)
+# 	puts b
+#  end
+#     return b
+
+# puts ruby1(22,45,89)
+
+# *************sort method****************
+
+k = [23,45,67,89]
+# puts k.sort
+
+# k.sort! { |first_num, second_num| first_num <=> second_num }   # incre 
+# k.sort! { |first_num, second_num| second_num <=> first_num }
+
+puts k.sort.reverse
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
